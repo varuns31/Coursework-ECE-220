b7c74c3ed19ab9b2274b4672576c11bdb33d5932
Varun <varuns7@illinois.edu>
2021-09-17 14:43:06 -0500

    MP3 COMPLETE

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -8,7 +8,20 @@ int main()
   printf("Enter a row index: ");
   scanf("%d",&row);
 
-  // Write your code here
+  int n=row+1;
+  for(int i=0;i<n;i++)
+    {
+      int unsigned long long term=1;
+      for(int j=1;j<=i;j++)
+	{
+	  if(i==0)continue;
+	  else 
+	    {
+	      term=(term*(row+1-j))/j;
+	    }
+	}
+      printf("%lld ",term);
+    }
 
   return 0;
 }
